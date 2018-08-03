6daa94fb65fd958dedfea9f1b1a85baae81c06e1
Jeffrey Ye <lightning.x.revenant@gmail.com>
2018-08-03 12:13:45 -0700

    fix breaking changes

--- src/AvaloniaEdit/Editing/AbstractMargin.cs
+++ src/AvaloniaEdit/Editing/AbstractMargin.cs
@@ -38,10 +38,10 @@ namespace AvaloniaEdit.Editing
 
         public AbstractMargin()
         {
-            this.GetPropertyChangedObservable(TextViewProperty).Subscribe(o =>
+            this.GetObservableWithHistory(TextViewProperty).Subscribe(o =>
             {
                 _wasAutoAddedToTextView = false;
-                OnTextViewChanged(o.OldValue as TextView, o.NewValue as TextView);
+                OnTextViewChanged(o.Item1, o.Item2);
             });
         }
 
